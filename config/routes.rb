6d0cd5f359e378793d4259291fcaf4b9e66b2583
a1153tm <a1153tm@aiit.ac.jp>
2012-05-02 00:56:32 +0900

    chapter C

--- config/routes.rb
+++ config/routes.rb
@@ -51,8 +51,8 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
   # See how all your routes lay out with "rake routes"
+  root to: 'store#index', as: 'store'
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
